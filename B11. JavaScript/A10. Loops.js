f1c5e1e40a87638f848297d6563d60101d45ca27
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-06-01 11:51:24 +0530

    Create A10. Loops.js

---
+++ B11. JavaScript/A10. Loops.js
@@ -0,0 +1,30 @@
+while Loop:
+  
+var i= 1;
+
+while( i<= 5){
+  console.log(i);
+  i++;
+}
+
+Output:
+1
+2
+3
+4
+5
+
+for loop:
+  
+  for( var i =1; i<= 5; i++){
+  console.log(i);
+}
+
+Do while loop:
+  
+var i= 1;
+
+do{
+  console.log(i);
+  i++;
+}while(i <= 5);
