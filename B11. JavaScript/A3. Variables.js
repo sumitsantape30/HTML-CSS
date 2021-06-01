e6f4cc844b37a1ea459a1e024c892882af401f3c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-06-01 09:38:58 +0530

    Create A3. Variables.js

---
+++ B11. JavaScript/A3. Variables.js
@@ -0,0 +1,20 @@
+
+JS is dynamic, you dont need to specify the variable type while intialising
+
+var a= 10;
+
+//we dont specify the type if the numberic value, as now a is holding the number 10 now I can make hold the string
+a= "Sumit"
+//I can also give it a boolean
+a= true
+// Adding semicolon after every statement in java script is optional.
+
+The build in data values that we have, so there are 6 primitive values and there is 1 which is object.
+In primitive values we have: i) Number (umber refers to both  integeral and decimal number)
+ii) String  iii) Boolean iv) null v)undefined vi) Symbol
+
+// to print the statement
+var a= 10;
+console.log(a)
+
+
