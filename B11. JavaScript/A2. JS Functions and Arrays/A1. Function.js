7dfe14fb293e10760bd277ab8cf8092b5646dec2
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-06-02 17:35:27 +0530

    Create A1. Function.js

---
+++ B11. JavaScript/A2. JS Functions and Arrays/A1. Function.js
@@ -0,0 +1,26 @@
+// In order to create a function write function keyword and then write function name and then curly brackets to indicate start and end of the function
+function showAlert(){
+    // so this function will show an alert
+    alert("Hey!");
+}
+
+//next thing we have to do is call the function
+// we use camel case while naming function, first letter will be small and then first letter of every word will be capital
+showAlert();
+// so this will show and alert msg
+===================================================
+  
+// If I want to take the alert message from user
+  
+  function showAlert(msg){ //you dont need to put the integer type here, writing var wil through an error
+ //The value that function receives is called are called parameters
+    alert(msg);
+}
+
+showAlert("Hey"); //now while calling the function I'll pass the message I want to display
+     // the value that we pass through the function while calling it re called argument
+
+==========================================================
+  
+  // you can also create a function that takes more that one input as well
+  
