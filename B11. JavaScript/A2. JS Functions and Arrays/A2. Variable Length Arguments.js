bbce06bf88e104aa4be68f398bf45ece1c4aea56
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-24 08:15:22 +0530

    Update A2. Variable Length Arguments.js

--- B11. JavaScript/A2. JS Functions and Arrays/A2. Variable Length Arguments.js
+++ B11. JavaScript/A2. JS Functions and Arrays/A2. Variable Length Arguments.js
@@ -1,1 +1,11 @@
+function getSum(a, b){
 
+    console.log(a, b); //this will print value of a only as we have passed single value, aur b keliye undefined print karega
+    return a+b; //here we are trying to add 6 with undefined that's why its giving NaN.
+}
+
+let result= getSum(6); //we have passed only on1 value, in java/cpp it'll throw a error but js is forgiving, it'll print NaN
+console.log(result);
+
+getSum(6, 7, 8); // the function need only two values but we are passing 3 values \, the extra argument will be simple ignored and it'll return the sum of first two values
+//this will give output: 13
