fdf4e80c1e5db2593428cb8312b75d2a5eb21f6c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-25 06:30:48 +0530

    Create A1. Objects.js

---
+++ B11. JavaScript/A3. Objects And Timing Events/A1. Objects.js
@@ -0,0 +1,21 @@
+//var students = {}; // this is an empty object, you can add properties as well
+var students = {
+    name: "abc",
+    rollNo: 135,
+     marks: 90
+}; // here keys can only be string, you can put like "name" "rollNo" etc.
+
+//now we have particular properties as well as values
+
+//If I want to access one particular property of this object, the notation i'll use is dot
+console.log(students.name);
+students.name = "Sumit";
+console.log(students);
+
+//If I try to access the property which I have not defined in the object then JS will give undefined, java/cpp gives error
+console.log(students.sem);
+
+students.sem= 5; // no this object will have this property, its completely flexible to add any key anytime
+console.log(students);
+
+//you can have functions as value as well
