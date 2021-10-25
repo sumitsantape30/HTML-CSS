fd1746370d273307016365a92af8d688fcd9baa0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-25 07:54:48 +0530

    Create A4. How are objects stored.js

---
+++ B11. JavaScript/A3. Objects And Timing Events/A4. How are objects stored.js
@@ -0,0 +1,20 @@
+var obj= {
+    "p1": 10
+}
+
+var obj2= obj; // this is just another variable that is refering to the old object
+
+console.log(obj);
+console.log(obj2);
+
+obj2.p1= 100;
+console.log(obj2);
+console.log(obj); 
+
+console.log(obj == obj2);
+
+console.log(obj === obj2);
+
+obj2 = {"p1": 100}; // now this obj2 is refering to the other object
+
+console.log(obj == obj2); // obj and obj2 are refering to two different objects in a memory
