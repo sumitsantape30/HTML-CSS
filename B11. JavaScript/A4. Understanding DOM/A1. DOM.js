c1f6f7fcf9ec690c8ef7b77a18fbed50cfe22825
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-25 20:58:11 +0530

    Create A1. DOM.js

---
+++ B11. JavaScript/A4. Understanding DOM/A1. DOM.js
@@ -0,0 +1,12 @@
+// with the help of Js we'll be able to add interactivity and behaviour to our webpages
+// DOM: Document object Model: It represents the logical structure of the page that we have.
+
+                   document
+                   |--------HTML (this document will have HTML tag at the top)
+                             |-------head (this html has head and body)
+                             |-------Body (this body will have different section
+                                      |------h1      _______ 
+                                      |------div----|  (this div will have different sections)
+                                      |------p       
+                         
+  //all these elements are part of DOM and we'll be able to manupulate, access using DOM, js is able to tht using DOM
